8bb30cb4ccad42eef240df4218c4a43d564f53d2
Kasper <kasperdtb@gmail.com>
2023-03-28 20:01:23 +0200

    Change
    Change

---
+++ .ipynb_checkpoints/customDataset-checkpoint.py
@@ -0,0 +1,28 @@
+# https://www.youtube.com/watch?v=ZoZHd0Zm3RY&ab_channel=AladdinPersson
+
+import os
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from skimage import io
+
+class LBSTDataset(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.annotations) # total sample size
+    
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0]) # 0 indicates 1st column of the csv file. os.path.join() method in Python join one or more path components intelligently. This method concatenates various path components with exactly one directory separator (‘/’) following each non-empty part except the last path component. If the last path component to be joined is empty then a directory separator (‘/’) is put at the end. 
+        image = io.imread(img_path)
+        y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
+        
+        if self.transform:
+            image = self.transform(image) # optional: if we send in trasnforms, it will transform images
+            
+        return (image, y_label)
+    
+# This class loads 1 image and the corresponding class to that image.
