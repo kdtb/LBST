b8f62064bd60c968ddbfe7ed8c317786698714e2
Kasper <kasperdtb@gmail.com>
2023-04-05 15:37:30 +0200

    ed
    ed

--- .ipynb_checkpoints/customDataModule-checkpoint.py
+++ .ipynb_checkpoints/customDataModule-checkpoint.py
@@ -23,8 +23,8 @@ class CustomDataModule(pl.LightningDataModule):
             transform=transforms.Compose(
                 [
                     transforms.Resize((224, 224)),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.RandomVerticalFlip(),
+                   # transforms.RandomHorizontalFlip(),
+                    #transforms.RandomVerticalFlip(),
                     #transforms.RandomResizedCrop(224),
                     transforms.ToTensor(),  # ,
                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # Using the mean and std of Imagenet is a common practice.  
